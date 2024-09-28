7339926c23ab81af769a782acfea3f6be59291f5
hung0125 <65654501+hung0125@users.noreply.github.com>
2024-09-28 17:27:04 +0800

    Add files via upload

--- sdock.py
+++ sdock.py
@@ -138,168 +138,173 @@ def base_stock_anal(stock_idx_or_name):
 
     filter_list = []
     cnt = 0
+    
     for S in stocks[stock_c]:
-        sleep(0.25)
-        cnt += 1
-        root.update_idletasks()
-        resp = rq.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{S}?range=1y&interval=1d&indicators=quote&includeTimestamps=true&corsDomain=finance.yahoo.com", headers=header)
-        resp_m = rq.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{S}?range=10y&interval=1mo&indicators=quote&includeTimestamps=true&corsDomain=finance.yahoo.com", headers=header)
-        dat = loads(resp.text)
-        dat_m = loads(resp_m.text)['chart']['result'][0]
+        try:
+            sleep(0.25)
+            cnt += 1
+            root.update_idletasks()
+            resp = rq.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{S}?range=1y&interval=1d&indicators=quote&includeTimestamps=true&corsDomain=finance.yahoo.com", headers=header)
+            resp_m = rq.get(f"https://query1.finance.yahoo.com/v8/finance/chart/{S}?range=10y&interval=1mo&indicators=quote&includeTimestamps=true&corsDomain=finance.yahoo.com", headers=header)
+            dat = loads(resp.text)
+            dat_m = loads(resp_m.text)['chart']['result'][0]
 
-        # do monthly analysis
-        opens_m = dat_m['indicators']['quote'][0]['open']
-        opens_m.pop()
-        closes_m = dat_m['indicators']['quote'][0]['close']
-        closes_m.pop()
-        dat_m['timestamp'].pop()
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        m_gains = {}
-        for M in months:
-            m_gains[M] = [0] * 10
-        
-        for i in range(len(opens_m)):
-            p_op = opens_m[i]
-            p_cl = closes_m[i]
-            ts = ts2date(dat_m['timestamp'][i]).split() # d m y
-            # if ts[1] == 'Sep':
-            #     print(f'[{ts[1]}]{int(date_now[2])}-{int(ts[2])}-1 -> {p_op}, {p_cl}, {pchange(p_cl, p_op)}')
-            use_idx = int(date_now[2])-int(ts[2])
-            if use_idx == 10:
-                use_idx -= 1
-            m_gains[ts[1]][use_idx] += pchange(p_cl, p_op)
-
-        # do emas
-        ps = dat['chart']['result'][0]['indicators']['quote'][0]
-        closes = list(ps['close'])
-        emas_days = [5, 10, 20, 30, 50]
-        emas_line = []
-        emas_trades = []
-
-        for ED in emas_days:
-            windows = closes[:ED] # closed price in first ED days
-            emas_line.append([np.mean(np.array(windows))])
+            # do monthly analysis
+            opens_m = dat_m['indicators']['quote'][0]['open']
+            opens_m.pop()
+            closes_m = dat_m['indicators']['quote'][0]['close']
+            closes_m.pop()
+            dat_m['timestamp'].pop()
+            months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            m_gains = {}
+            for M in months:
+                m_gains[M] = [0] * 10
             
-            in_buy = False
-            cur_trades = []
-            wons = 0
-            losses = 0
-            gain_p = 0
-            buy_intervals = [] # days
-            last_buy_ts = dat['chart']['result'][0]['timestamp'][0]
-            last_sell_ts = -1
-            
-            for i in range(10, len(closes)): # day 11, really analyze
-                # buy signal
-                if closes[i] >= emas_line[-1][-1] and not in_buy:
-                    tx = getBaseTx(dat, emas_line, i)
-                    tx['price'] = closes[i]
-                    tx['buy'] = True
-                    in_buy = True
+            for i in range(len(opens_m)):
+                p_op = opens_m[i]
+                p_cl = closes_m[i]
+                ts = ts2date(dat_m['timestamp'][i]).split() # d m y
+                # if ts[1] == 'Sep':
+                #     print(f'[{ts[1]}]{int(date_now[2])}-{int(ts[2])}-1 -> {p_op}, {p_cl}, {pchange(p_cl, p_op)}')
+                use_idx = int(date_now[2])-int(ts[2])
+                if use_idx == 10:
+                    use_idx -= 1
+                m_gains[ts[1]][use_idx] += pchange(p_cl, p_op)
 
-                    ts_cur = dat['chart']['result'][0]['timestamp'][i]
-                    buy_intervals.append(calcDays(ts_cur, last_buy_ts))
-                    last_buy_ts = ts_cur
-                    last_sell_ts = -1
-                    cur_trades.append(tx)
-                # sell signal
-                if closes[i] < emas_line[-1][-1] and in_buy and len(cur_trades) > 0:
-                    tx = getBaseTx(dat, emas_line, i)
-                    tx['price'] = closes[i]
-                    
-                    change = pchange(closes[i], cur_trades[-1]['price'])
-                    tx['gain'] = round(change, 2)
-                    
-                    if change > 0:
-                        wons += 1
-                    else:
-                        losses += 1
-                    gain_p += change
-                    
-                    tx['buy'] = False
-                    in_buy = False
-                    cur_ts = dat['chart']['result'][0]['timestamp'][i]
-                    if cur_ts >= last_buy_ts:
-                        last_sell_ts = cur_ts
-                    cur_trades.append(tx)
-                    
+            # do emas
+            ps = dat['chart']['result'][0]['indicators']['quote'][0]
+            closes = list(ps['close'])
+            emas_days = [5, 10, 20, 30, 50]
+            emas_line = []
+            emas_trades = []
 
-                windows.pop(0)
-                windows.append(closes[i])
-                # add ema value
-                emas_line[-1].append(np.mean(np.array(windows)))
+            for ED in emas_days:
+                windows = closes[:ED] # closed price in first ED days
+                emas_line.append([np.mean(np.array(windows))])
                 
+                in_buy = False
+                cur_trades = []
+                wons = 0
+                losses = 0
+                gain_p = 0
+                buy_intervals = [] # days
+                last_buy_ts = dat['chart']['result'][0]['timestamp'][0]
+                last_sell_ts = -1
+                
+                for i in range(10, len(closes)): # day 11, really analyze
+                    # buy signal
+                    if closes[i] >= emas_line[-1][-1] and not in_buy:
+                        tx = getBaseTx(dat, emas_line, i)
+                        tx['price'] = closes[i]
+                        tx['buy'] = True
+                        in_buy = True
 
-            emas_trades.append({'trades': cur_trades, 
-                                'wons': wons, 
-                                'losses': losses, 
-                                'gainp': gain_p, 
-                                'avgbuydays': int(np.mean(np.array(buy_intervals))),
-                                'lastbuyts': [last_buy_ts, last_sell_ts]})
-        
+                        ts_cur = dat['chart']['result'][0]['timestamp'][i]
+                        buy_intervals.append(calcDays(ts_cur, last_buy_ts))
+                        last_buy_ts = ts_cur
+                        last_sell_ts = -1
+                        cur_trades.append(tx)
+                    # sell signal
+                    if closes[i] < emas_line[-1][-1] and in_buy and len(cur_trades) > 0:
+                        tx = getBaseTx(dat, emas_line, i)
+                        tx['price'] = closes[i]
+                        
+                        change = pchange(closes[i], cur_trades[-1]['price'])
+                        tx['gain'] = round(change, 2)
+                        
+                        if change > 0:
+                            wons += 1
+                        else:
+                            losses += 1
+                        gain_p += change
+                        
+                        tx['buy'] = False
+                        in_buy = False
+                        cur_ts = dat['chart']['result'][0]['timestamp'][i]
+                        if cur_ts >= last_buy_ts:
+                            last_sell_ts = cur_ts
+                        cur_trades.append(tx)
+                        
 
-        if not filter_mode:
-            prt(f"Results - {S} (${dat['chart']['result'][0]['meta']['regularMarketPrice']}) From {ts2date(dat['chart']['result'][0]['timestamp'][0])} to {ts2date(dat['chart']['result'][0]['timestamp'][-1])}:\n")
-        
-        res = [] # summary table
-        winr_ma5 = 0
-        winr_ma10 = 0
-        
-        for i in range(len(emas_trades)):
-            wr = emas_trades[i]['wons']/(emas_trades[i]['wons']+emas_trades[i]['losses'])*100
-            if emas_days[i] == 5:
-                winr_ma5 = wr
-            elif emas_days[i] == 10:
-                winr_ma10 = wr
+                    windows.pop(0)
+                    windows.append(closes[i])
+                    # add ema value
+                    emas_line[-1].append(np.mean(np.array(windows)))
+                    
+
+                emas_trades.append({'trades': cur_trades, 
+                                    'wons': wons, 
+                                    'losses': losses, 
+                                    'gainp': gain_p, 
+                                    'avgbuydays': int(np.mean(np.array(buy_intervals))),
+                                    'lastbuyts': [last_buy_ts, last_sell_ts]})
             
-            data_tup = (
-                S,
-                emas_days[i],
-                f"{emas_trades[i]['wons']}/{emas_trades[i]['losses']} [{round(wr, 2)}%]",
-                round(emas_trades[i]['gainp'], 2),
-                round(emas_line[i][-1], 2),
-                str(emas_trades[i]['avgbuydays']) + ' days',
-                ts2date(emas_trades[i]['lastbuyts'][0]) + ('-' + ts2date(emas_trades[i]['lastbuyts'][1]) if emas_trades[i]['lastbuyts'][1] > 0 else '')
-            )
-            output_table.insert("", "end", values=data_tup)
-            res.append(list(data_tup))
 
-        output_table.insert("", "end", values=('_' * 5,) * 8)
-
-        if filter_mode:
-            use_idx = 0 if winr_ma5 >= winr_ma10 else 1
-            reg_p = dat['chart']['result'][0]['meta']['regularMarketPrice']
-            month_now = m_gains[date_now[1]]
-            month_nxt = m_gains[nxt_mth[date_now[1]]]
-            res[use_idx][0] = S
-            res[use_idx][5] = f'{reg_p} [{round(pchange(reg_p, res[use_idx][4]), 2)}%]'
-            res[use_idx].append(f'{round(np.mean(month_now), 1)}({m_gain_confd(month_now)})->{round(np.mean([month_nxt]), 1)}({m_gain_confd(month_nxt)})')
-            res[use_idx].append(emas_trades[use_idx]['lastbuyts'][0])
-            filter_list.append(res[use_idx])
-            prt(f"Analyzed: {S} [{cnt}/{len(stocks[stock_c])}]\n")
-        
-        # detail mode
-        res.append([len(emas_trades)+1, 'Quit', '-', '-', '-', '-', '-'])
-        # print(tabulate(res, headers=['Choice', 'EMA D.', 'Win/Lose', 'Gain %', 'Last EMA $', 'Avg Buy Intv.', 'Last Trade'], tablefmt='mixed_grid'))
-        mo_analysis(m_gains, S)
-
-        for k in range(5):
-            for T in emas_trades[k]['trades']:
-                tmp_date = ts2date(T["date"])
-                tmp_mth_gain = m_gains[tmp_date.split()[1]]
-                trade_details.append([
+            if not filter_mode:
+                prt(f"Results - {S} (${dat['chart']['result'][0]['meta']['regularMarketPrice']}) From {ts2date(dat['chart']['result'][0]['timestamp'][0])} to {ts2date(dat['chart']['result'][0]['timestamp'][-1])}:\n")
+            
+            res = [] # summary table
+            winr_ma5 = 0
+            winr_ma10 = 0
+            
+            for i in range(len(emas_trades)):
+                wr = emas_trades[i]['wons']/(emas_trades[i]['wons']+emas_trades[i]['losses'])*100
+                if emas_days[i] == 5:
+                    winr_ma5 = wr
+                elif emas_days[i] == 10:
+                    winr_ma10 = wr
+                
+                data_tup = (
                     S,
-                    emas_days[k],
-                    tmp_date,
-                    "buy" if T["buy"] else "sell",
-                    round(T["ema"], 2),
-                    f'{round(T["price"], 2)} [{round(pchange(T["price"], T["ema"]), 2)}%]',
-                    '↓' if T['buy'] else T['gain'],
-                    f'{round(np.mean(tmp_mth_gain), 1)} ({m_gain_confd(tmp_mth_gain)})'
-                ])
+                    emas_days[i],
+                    f"{emas_trades[i]['wons']}/{emas_trades[i]['losses']} [{round(wr, 2)}%]",
+                    round(emas_trades[i]['gainp'], 2),
+                    round(emas_line[i][-1], 2),
+                    str(emas_trades[i]['avgbuydays']) + ' days',
+                    ts2date(emas_trades[i]['lastbuyts'][0]) + ('-' + ts2date(emas_trades[i]['lastbuyts'][1]) if emas_trades[i]['lastbuyts'][1] > 0 else '')
+                )
+                output_table.insert("", "end", values=data_tup)
+                res.append(list(data_tup))
 
-        # progress
-        pb['value'] += 1/len(stocks[stock_c]) * 100
+            output_table.insert("", "end", values=('_' * 5,) * 8)
+
+            if filter_mode:
+                use_idx = 0 if winr_ma5 >= winr_ma10 else 1
+                reg_p = dat['chart']['result'][0]['meta']['regularMarketPrice']
+                month_now = m_gains[date_now[1]]
+                month_nxt = m_gains[nxt_mth[date_now[1]]]
+                res[use_idx][0] = S
+                res[use_idx][5] = f'{reg_p} [{round(pchange(reg_p, res[use_idx][4]), 2)}%]'
+                res[use_idx].append(f'{round(np.mean(month_now), 1)}({m_gain_confd(month_now)})->{round(np.mean([month_nxt]), 1)}({m_gain_confd(month_nxt)})')
+                res[use_idx].append(emas_trades[use_idx]['lastbuyts'][0])
+                filter_list.append(res[use_idx])
+                prt(f"Analyzed: {S} [{cnt}/{len(stocks[stock_c])}]\n")
+            
+            # detail mode
+            res.append([len(emas_trades)+1, 'Quit', '-', '-', '-', '-', '-'])
+            # print(tabulate(res, headers=['Choice', 'EMA D.', 'Win/Lose', 'Gain %', 'Last EMA $', 'Avg Buy Intv.', 'Last Trade'], tablefmt='mixed_grid'))
+            mo_analysis(m_gains, S)
+
+            for k in range(5):
+                for T in emas_trades[k]['trades']:
+                    tmp_date = ts2date(T["date"])
+                    tmp_mth_gain = m_gains[tmp_date.split()[1]]
+                    trade_details.append([
+                        S,
+                        emas_days[k],
+                        tmp_date,
+                        "buy" if T["buy"] else "sell",
+                        round(T["ema"], 2),
+                        f'{round(T["price"], 2)} [{round(pchange(T["price"], T["ema"]), 2)}%]',
+                        '↓' if T['buy'] else T['gain'],
+                        f'{round(np.mean(tmp_mth_gain), 1)} ({m_gain_confd(tmp_mth_gain)})'
+                    ])
+
+            # progress
+            pb['value'] += 1/len(stocks[stock_c]) * 100
+        except:
+            prt("Error processing " + S)
+            pb['value'] += 1/len(stocks[stock_c]) * 100
     
     # do filter mode stuff
     if filter_mode:
